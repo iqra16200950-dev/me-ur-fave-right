87110aa4eeb860cee01c7772803af6097c04bc76
Iqra Fathima <iqra16200950@gmail.com>
2026-02-14 11:56:50 +0000

    try

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-//Elements
-const envelope = document.getElementById("envelop-container");
+// Elements
+const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
 const noBtn = document.querySelector(".no-btn");
-const yesbtn = document.querySelector(".btn[alt='Yes']");
+const yesBtn = document.querySelector(".btn[alt='Yes']");
 
 const title = document.getElementById("letter-title");
 const catImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 
-//Click Envelope
+// Click Envelope
 
 envelope.addEventListener("click", () => {
     envelope.style.display = "none";
@@ -20,31 +20,31 @@ envelope.addEventListener("click", () => {
     },50);
 });
 
-//Logic to move the NO btn
+// Logic to move the NO btn
 
-noBtn.addEventListener("mouseover",() => {
+noBtn.addEventListener("mouseover", () => {
     const min = 200;
     const max = 200;
 
-    const distance = Math.random() * (max-min) + min;
+    const distance = Math.random() * (max - min) + min;
     const angle = Math.random() * Math.PI * 2;
 
     const moveX = Math.cos(angle) * distance;
     const moveY = Math.sin(angle) * distance;
 
     noBtn.style.transition = "transform 0.3s ease";
-     noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
 });
 
-//Logic to make YES btn grow
+// Logic to make YES btn to grow
 
-//let yesScale = 1;
+// let yesScale = 1;
 
-//yesBtn.style.position = "relative"
-//yesBtn.style.transformOrigin = "center center";
-//yesBtn.style.transition = "transform 0.3s ease";
+// yesBtn.style.position = "relative"
+// yesBtn.style.transformOrigin = "center center";
+// yesBtn.style.transition = "transform 0.3s ease";
 
-//noBtn.addEventListener("click", () => {
+// noBtn.addEventListener("click", () => {
 //     yesScale += 2;
 
 //     if (yesBtn.style.position !== "fixed") {
